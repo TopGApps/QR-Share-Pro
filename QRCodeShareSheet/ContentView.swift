2fc17d0e9d5f3bc8fb3ba7a35dca77322b32e457
Vaibhav Satishkumar <78756662+Visual-Studio-Coder@users.noreply.github.com>
2024-02-29 17:31:35 -0800

    Initial Commit

---
+++ QRCodeShareSheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRCodeShareSheet
+//
+//  Created by Vaibhav Satishkumar on 2/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRCodeShareSheet/QRCodeShareSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeShareSheetApp.swift
+//  QRCodeShareSheet
+//
+//  Created by Vaibhav Satishkumar on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeShareSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
