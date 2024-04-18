a4e9b23bbcb2d8188567d3eda14abcf12c9480ff
Vaibhav Satishkumar <78756662+Visual-Studio-Coder@users.noreply.github.com>
2024-04-17 17:18:51 -0700

    small UI changes

--- QRSharePro/HistoryDetailInfo.swift
+++ QRSharePro/HistoryDetailInfo.swift
@@ -885,54 +885,11 @@ struct HistoryDetailInfo: View {
                 }
             }
             ToolbarItem(placement: .topBarTrailing) {
-                Menu {
-                    Button {
-                        if qrCode.text.count > 3000 {
-                            showExceededLimitAlert = true
-                        } else {
-                            PHPhotoLibrary.requestAuthorization(for: .addOnly) { status in
-                                if status == .denied {
-                                    showPermissionsError = true
-                                } else {
-                                    UIImageWriteToSavedPhotosAlbum(qrCodeImage, nil, nil, nil)
-                                    showSavedAlert = true
-                                }
-                            }
-                        }
-                    } label: {
-                        Label("Save to Photos", systemImage: "square.and.arrow.down")
-                    }
-                    .disabled(qrCode.text.isEmpty)
-                    
-                    Button {
-                        if let idx = qrCodeStore.indexOfQRCode(withID: qrCode.id) {
-                            withAnimation {
-                                qrCodeStore.history[idx].pinned.toggle()
-                                qrCode.pinned.toggle()
-                                
-                                Task {
-                                    do {
-                                        try await save()
-                                    } catch {
-                                        print(error.localizedDescription)
-                                    }
-                                }
-                            }
-                        }
-                    } label: {
-                        Label(qrCode.pinned ? "Unpin" : "Pin", systemImage: qrCode.pinned ? "pin.slash.fill" : "pin")
-                    }
-                    
-                    Divider()
-                    
                     Button(role: .destructive) {
                         showingDeleteConfirmation = true
                     } label: {
                         Label("Delete", systemImage: "trash")
                     }
-                } label: {
-                    Label("Options", systemImage: "ellipsis.circle")
-                }
             }
             ToolbarItem(placement: .topBarTrailing) {
                 Menu {
